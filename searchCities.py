87a1f58c925dc8b1c5dd16a8be10ec71a484c16c
Luis Fernando <luis.silva9902@gmail.com>
2022-08-21 20:44:54 -0300

    Add new tests

--- searchCities.py
+++ searchCities.py
@@ -1,6 +1,14 @@
 cityList = ['Paris', 'Budapeste', 'Skopje', 'Rotterdam', 'Valencia', 'Vancouver', 'Amsterdam', 
 'Vienna', 'Sydney', 'New York City', 'London', 'Bangkok', 'Hong Kong', 'Dubai', 'Rome', 'Istanbul']
 
+citySearch = []
+
 def searchCities(search):
   if len(search) < 2:
     return False
+  
+  for x in cityList:
+    if x.startswith(search):
+      citySearch.append(x)
+  
+  return citySearch
--- tests/test_searchCities.py
+++ tests/test_searchCities.py
@@ -1,4 +1,7 @@
 from searchCities import searchCities
 
 def test_should_return_empty_for_searches_with_less_than_two_characters():
-  assert searchCities('a') == False 
+  assert searchCities('a') == False
+
+def test_should_return_search_results():
+  assert searchCities('Va') == ['Valencia', 'Vancouver']
