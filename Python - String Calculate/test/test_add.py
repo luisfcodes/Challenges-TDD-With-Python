5bad03c36868054a1e82e7589091bb325e77cf7b
Luis Fernando <luis.silva9902@gmail.com>
2022-08-23 14:23:01 -0300

    Finalization of the project, addition of the final tests

--- Python - String Calculate/test/test_add.py
+++ Python - String Calculate/test/test_add.py
@@ -19,4 +19,13 @@ def test_should_return_the_sum_accepting_new_delimiters():
     assert add("//;\n1;3") == 4
 
 def test_should_return_an_error_if_the_delimiter_is_different_from_the_one_informed():
-    assert add("//|\n1|2,3") == "'|' esperado mas ',' encontrado na posição 3."
+    assert add("//|\n1|2,3") == "'|' esperado, mas ',' encontrado na posição 3."
+
+def test_should_return_an_error_message_if_it_receives_negative_numbers():
+    assert add("2,-4,-9") == "Número(s) negativo(s) não permitidos: -4,-9"
+
+def test_should_return_all_error_messages_found_in_the_string():
+    assert add("//|\n1|2,-3") == "Número(s) negativo(s) não permitidos: -3\n'|' esperado, mas ',' encontrado na posição 3."
+
+def test_should_return_the_sum_of_the_numbers_ignoring_values_above_1000():
+    assert add("2,1001") == 2
